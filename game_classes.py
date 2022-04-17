568f0f6114f24443a1adf9784a975ff603fc8820
alexreute <reute@gmx.net>
2022-04-17 23:54:23 +0200

    Initial commit

---
+++ game_classes.py
@@ -0,0 +1,9 @@
+
+
+class GameClass():
+    def __init__(self):
+        pass
+
+    def start(self):
+        pass
+
---
+++ gui.py
@@ -0,0 +1,6 @@
+import random
+from tkinter import *
+from tkinter import ttk
+from time import sleep
+from tkinter import messagebox
+
